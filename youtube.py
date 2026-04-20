34e68c086c3177428f04f483fe2481b878304f0f
Toria <victoriatetteh67@gmail.com>
2026-04-20 23:29:05 +0000

    Rename Aiden_Age to Kay_Age in age check

--- youtube.py
+++ youtube.py
@@ -1,9 +1,9 @@
-Aiden_Age = 10
+Kay_Age = 10
 Age_in_Class_3 = 7
 
-if Aiden_Age < Age_in_Class_3: 
-    print("Aiden should be in Class 2")
-elif Aiden_Age == Age_in_Class_3:
+if Kay_Age < Age_in_Class_3: 
+    print("Kay should be in Class 2")
+elif Kay_Age == Age_in_Class_3:
     print("Enjoy class 2")
 else:
-    print("Aiden should be in another class")
+    print("Kay should be in another class")
