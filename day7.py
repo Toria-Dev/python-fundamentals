49430c9e31168437dcf0eac8d0ad6b2621f10506
Toria <victoriatetteh67@gmail.com>
2026-04-20 23:24:35 +0000

    Add Day 7 - Review and Catch Up

---
+++ day7.py
@@ -0,0 +1,4 @@
+print('Hello, World') # making it a single quotation instead of 1 - still worked
+print ("Tomorrow") # leaving out one parenthisis #it wouldn't let me keep going
+2++2 #What happens if you put a plus sign before a number? What about 2++2?
+(42*60) + 42 
